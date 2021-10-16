94ca8532738c6ece2f885c0fc33d509aabc97978
ToniSahmir <sahmir.toni66@gmail.com>
2021-10-16 23:44:24 +0700

    membuat tampilan dashboard dan menambahkan gambar pada konten dashboard

---
+++ app/Views/pangan/dashboard.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dashboard | Ketersediaan Pangan</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition layout-top-nav">
+  <div class="wrapper">
+
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+      <div class="container">
+        <a href="/" class="navbar-brand">
+          <img src="/img/SIKP.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+          <span class="brand-text font-weight-light">SInKePang</span>
+        </a>
+
+        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <!-- Right navbar links -->
+        <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
+          <li class="nav-item">
+            <a href="index3.html" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Data Pangan</a>
+          </li>
+          <!-- profile Dropdown Menu -->
+          <li class="nav-item dropdown user-menu">
+            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+              <img src="/img/logo_portal.png" class="user-image img-circle elevation-2" alt="User Image">
+              <span class="d-none d-md-inline">FASILKOM</span>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+              <!-- User image -->
+              <li class="user-header bg-info">
+                <img src="/img/logo_portal.png" class="img-circle elevation-2" alt="User Image">
+                <p>
+                  Fakultas Ilmu komputer
+                  <small>Member since Oct. 2021</small>
+                </p>
+              </li>
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                <a href="#" class="btn btn-default btn-flat float-right">Sign out</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0"> Aplikasi Ketersediaan Pangan</small></h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Beranda</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <div class="content">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3">
+              <a href="#">
+                <div class="card bg-info">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/img/pngwing.com (13).png">
+                    </div>
+                    <h3 class="profile-username text-center">Sayuran</h3>
+                  </div>
+                </div>
+              </a>
+
+              <a href="#">
+                <div class="card bg-primary">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/img/pngwing.com (4).png">
+                    </div>
+                    <h3 class="profile-username text-center">Sapi</h3>
+                  </div>
+                </div>
+              </a>
+
+              <a href="#">
+                <div class="card bg-secondary">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/dist/img/user4-128x128.jpg">
+                    </div>
+                    <h3 class="profile-username text-center">Kambing</h3>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-lg-3">
+              <a href="#">
+                <div class="card bg-warning">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/dist/img/user4-128x128.jpg">
+                    </div>
+                    <h3 class="profile-username text-center">Domba</h3>
+                  </div>
+                </div>
+              </a>
+
+              <a href="#">
+                <div class="card bg-danger">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/img/pngwing.com (11).png">
+                    </div>
+                    <h3 class="profile-username text-center">Jagung</h3>
+                  </div>
+                </div>
+              </a>
+
+              <a href="#">
+                <div class="card bg-success">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/dist/img/user4-128x128.jpg">
+                    </div>
+                    <h3 class="profile-username text-center">Buah-buahan</h3>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-lg-3">
+              <a href="#">
+                <div class="card bg-indigo">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/dist/img/user4-128x128.jpg">
+                    </div>
+                    <h3 class="profile-username text-center">Ayam</h3>
+                  </div>
+                </div>
+              </a>
+
+              <a href="#">
+                <div class="card bg-navy">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/img/pngwing.com (14).png">
+                    </div>
+                    <h3 class="profile-username text-center">Beras</h3>
+                  </div>
+                </div>
+              </a>
+
+              <a href="#">
+                <div class="card bg-fuchsia">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/img/pngwing.com (10).png">
+                    </div>
+                    <h3 class="profile-username text-center">Umbi-umbian</h3>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-lg-3">
+              <a href="#">
+                <div class="card bg-teal">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/img/pngwing.com (9).png">
+                    </div>
+                    <h3 class="profile-username text-center">Kacang-kacangan</h3>
+                  </div>
+                </div>
+              </a>
+
+              <a href="#">
+                <div class="card bg-lime">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/img/pngwing.com (7).png">
+                    </div>
+                    <h3 class="profile-username text-center">Ikan</h3>
+                  </div>
+                </div>
+              </a>
+
+              <a href="#">
+                <div class="card bg-orange">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img class="img-fluid" src="/img/pngwing.com (8).png">
+                    </div>
+                    <h3 class="profile-username text-center">Telur</h3>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Main Footer -->
+    <footer class="main-footer">
+      <!-- To the right -->
+      <div class="float-right d-none d-sm-inline">
+        Anything you want
+      </div>
+      <!-- Default to the left -->
+      <strong>Copyright &copy; 2021 <a href="/">Ketahanan Pangan</a>.</strong> All rights reserved.
+    </footer>
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- REQUIRED SCRIPTS -->
+
+  <!-- jQuery -->
+  <script src="/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="/dist/js/demo.js"></script>
+</body>
+
+</html>
