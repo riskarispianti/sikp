26c7eabcd26fab32da1b9d87885ee8219b94bbc8
ToniSahmir <sahmir.toni66@gmail.com>
2021-10-19 20:45:59 +0700

    membuat file login dengan controllernya dan membuat navbar pada folder layout

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -21,53 +21,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <body class="hold-transition layout-top-nav">
   <div class="wrapper">
 
-    <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
-      <div class="container">
-        <a href="/" class="navbar-brand">
-          <img src="/img/SIKP.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-          <span class="brand-text font-weight-light">SInKePang</span>
-        </a>
-
-        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-
-        <!-- Right navbar links -->
-        <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
-          <li class="nav-item">
-            <a href="index3.html" class="nav-link">Home</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Data Pangan</a>
-          </li>
-          <!-- profile Dropdown Menu -->
-          <li class="nav-item dropdown user-menu">
-            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-              <img src="/img/logo_portal.png" class="user-image img-circle elevation-2" alt="User Image">
-              <span class="d-none d-md-inline">FASILKOM</span>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-              <!-- User image -->
-              <li class="user-header bg-info">
-                <img src="/img/logo_portal.png" class="img-circle elevation-2" alt="User Image">
-                <p>
-                  Fakultas Ilmu komputer
-                  <small>Member since Oct. 2021</small>
-                </p>
-              </li>
-              <!-- Menu Footer-->
-              <li class="user-footer">
-                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                <a href="#" class="btn btn-default btn-flat float-right">Sign out</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <!-- /.navbar -->
+    <?= $this->include('layout/navbar'); ?>
 
     <?= $this->renderSection('content'); ?>
 
