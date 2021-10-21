20ac53958cb9fc5596de7e8d026b7f17366a8e81
ToniSahmir <sahmir.toni66@gmail.com>
2021-10-21 09:41:25 +0700

    buat halaman profile dan sertaannya

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -36,7 +36,7 @@
           </li>
           <!-- Menu Footer-->
           <li class="user-footer">
-            <a href="#" class="btn btn-default btn-flat">Profile</a>
+            <a href="/profile" class="btn btn-default btn-flat">Profile</a>
             <a href="#" class="btn btn-default btn-flat float-right">Sign out</a>
           </li>
         </ul>
