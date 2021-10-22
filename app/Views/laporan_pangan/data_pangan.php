cb2813d73cc62693690099bdae3eff835d32e4aa
ToniSahmir <sahmir.toni66@gmail.com>
2021-10-22 22:10:03 +0700

    menambahkan tabel pada data pangan

--- app/Views/laporan_pangan/data_pangan.php
+++ app/Views/laporan_pangan/data_pangan.php
@@ -18,90 +18,148 @@
           </ol>
         </div><!-- /.col -->
       </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
+    </div>
   </div>
   <!-- /.content-header -->
 
   <section class="content">
     <div class="container">
       <div class="row">
-        <div class="col-md-9">
+        <div class="col-12">
           <div class="card">
             <div class="card-header">
-              <h3 class="card-title">Simple Full Width Table</h3>
-
-              <div class="card-tools">
-                <ul class="pagination pagination-sm float-right">
-                  <li class="page-item"><a class="page-link" href="#">«</a></li>
-                  <li class="page-item"><a class="page-link" href="#">1</a></li>
-                  <li class="page-item"><a class="page-link" href="#">2</a></li>
-                  <li class="page-item"><a class="page-link" href="#">3</a></li>
-                  <li class="page-item"><a class="page-link" href="#">»</a></li>
-                </ul>
-              </div>
+              <h3 class="card-title">DataTable with default features</h3>
             </div>
             <!-- /.card-header -->
-            <div class="card-body p-0">
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th style="width: 10px">#</th>
-                    <th>Task</th>
-                    <th>Progress</th>
-                    <th style="width: 40px">Label</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1.</td>
-                    <td>Update software</td>
-                    <td>
-                      <div class="progress progress-xs">
-                        <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                      </div>
-                    </td>
-                    <td><span class="badge bg-danger">55%</span></td>
-                  </tr>
-                  <tr>
-                    <td>2.</td>
-                    <td>Clean database</td>
-                    <td>
-                      <div class="progress progress-xs">
-                        <div class="progress-bar bg-warning" style="width: 70%"></div>
-                      </div>
-                    </td>
-                    <td><span class="badge bg-warning">70%</span></td>
-                  </tr>
-                  <tr>
-                    <td>3.</td>
-                    <td>Cron job running</td>
-                    <td>
-                      <div class="progress progress-xs progress-striped active">
-                        <div class="progress-bar bg-primary" style="width: 30%"></div>
-                      </div>
-                    </td>
-                    <td><span class="badge bg-primary">30%</span></td>
-                  </tr>
-                  <tr>
-                    <td>4.</td>
-                    <td>Fix and squish bugs</td>
-                    <td>
-                      <div class="progress progress-xs progress-striped active">
-                        <div class="progress-bar bg-success" style="width: 90%"></div>
-                      </div>
-                    </td>
-                    <td><span class="badge bg-success">90%</span></td>
-                  </tr>
-                </tbody>
-              </table>
+            <div class="card-body">
+              <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                <div class="row">
+                  <div class="col-sm-12 col-md-6">
+                    <div class="dt-buttons btn-group flex-wrap"> <button class="btn btn-secondary buttons-copy buttons-html5" tabindex="0" aria-controls="example1" type="button"><span>Copy</span></button> <button class="btn btn-secondary buttons-csv buttons-html5" tabindex="0" aria-controls="example1" type="button"><span>CSV</span></button> <button class="btn btn-secondary buttons-excel buttons-html5" tabindex="0" aria-controls="example1" type="button"><span>Excel</span></button> <button class="btn btn-secondary buttons-pdf buttons-html5" tabindex="0" aria-controls="example1" type="button"><span>PDF</span></button> <button class="btn btn-secondary buttons-print" tabindex="0" aria-controls="example1" type="button"><span>Print</span></button>
+                      <div class="btn-group"><button class="btn btn-secondary buttons-collection dropdown-toggle buttons-colvis" tabindex="0" aria-controls="example1" type="button" aria-haspopup="true" aria-expanded="false"><span>Column visibility</span></button></div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-md-6">
+                    <div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label></div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-12">
+                    <table id="example2" class="table table-bordered table-hover dataTable dtr-inline" role="grid" aria-describedby="example2_info">
+                      <thead>
+                        <tr role="row">
+                          <th class="sorting sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Rendering engine</th>
+                          <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Browser</th>
+                          <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Platform(s)</th>
+                          <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending">Engine version</th>
+                          <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">CSS grade</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr class="odd">
+                          <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
+                          <td>Firefox 1.0</td>
+                          <td>Win 98+ / OSX.2+</td>
+                          <td>1.7</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="even">
+                          <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
+                          <td>Firefox 1.5</td>
+                          <td>Win 98+ / OSX.2+</td>
+                          <td>1.8</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="odd">
+                          <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
+                          <td>Firefox 2.0</td>
+                          <td>Win 98+ / OSX.2+</td>
+                          <td>1.8</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="even">
+                          <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
+                          <td>Firefox 3.0</td>
+                          <td>Win 2k+ / OSX.3+</td>
+                          <td>1.9</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="odd">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Camino 1.0</td>
+                          <td>OSX.2+</td>
+                          <td>1.8</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="even">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Camino 1.5</td>
+                          <td>OSX.3+</td>
+                          <td>1.8</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="odd">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Netscape 7.2</td>
+                          <td>Win 95+ / Mac OS 8.6-9.2</td>
+                          <td>1.7</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="even">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Netscape Browser 8</td>
+                          <td>Win 98SE+</td>
+                          <td>1.7</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="odd">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Netscape Navigator 9</td>
+                          <td>Win 98+ / OSX.2+</td>
+                          <td>1.8</td>
+                          <td>A</td>
+                        </tr>
+                        <tr class="even">
+                          <td class="sorting_1 dtr-control">Gecko</td>
+                          <td>Mozilla 1.0</td>
+                          <td>Win 95+ / OSX.1+</td>
+                          <td>1</td>
+                          <td>A</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-12 col-md-5">
+                    <div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div>
+                  </div>
+                  <div class="col-sm-12 col-md-7">
+                    <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                      <ul class="pagination">
+                        <li class="paginate_button page-item previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
+                        <li class="paginate_button page-item active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
+                        <li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
+                        <li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
+                        <li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
+                        <li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
+                        <li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li>
+                        <li class="paginate_button page-item next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             <!-- /.card-body -->
           </div>
+          <!-- /.card -->
         </div>
         <!-- /.col -->
       </div>
       <!-- /.row -->
     </div>
+    <!-- /.container-fluid -->
   </section>
 </div>
 
