e4fcea20475ce8b0c287ef1f5f0ad60207102f5f
ToniSahmir <sahmir.toni66@gmail.com>
2021-10-23 08:32:25 +0700

    memakai hover dalam tabel

--- app/Views/laporan_pangan/data_pangan.php
+++ app/Views/laporan_pangan/data_pangan.php
@@ -9,7 +9,7 @@
     <div class="container">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0"> Laporan Ketersediaan Pangan</small></h1>
+          <h1 class="m-0">Laporan Ketersediaan Pangan</small></h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
@@ -27,12 +27,9 @@
       <div class="row">
         <div class="col-12">
           <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">DataTable with default features</h3>
-            </div>
             <!-- /.card-header -->
             <div class="card-body">
-              <table id="example1" class="table table-bordered table-striped">
+              <table id="example1" class="table table-bordered table-hover">
                 <thead>
                   <tr>
                     <th>Rendering engine</th>
@@ -451,15 +448,6 @@
                     <td>U</td>
                   </tr>
                 </tbody>
-                <tfoot>
-                  <tr>
-                    <th>Rendering engine</th>
-                    <th>Browser</th>
-                    <th>Platform(s)</th>
-                    <th>Engine version</th>
-                    <th>CSS grade</th>
-                  </tr>
-                </tfoot>
               </table>
             </div>
             <!-- /.card-body -->
