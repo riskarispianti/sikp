b845a98a39695b2fa15aaa25958ffcc0bf108093
Toni <sahmir.toni66@gmail.com>
2021-10-24 15:27:11 +0700

    menggunakan faker pada seed

--- app/Database/Seeds/ProfileSeeder.php
+++ app/Database/Seeds/ProfileSeeder.php
@@ -8,13 +8,24 @@ class ProfileSeeder extends Seeder
 {
     public function run()
     {
-        $data = [
-            'logo' => '/img/logo_portal.png',
-            'nama_dinas'    => 'Dinas Pertanian Subang',
-            'deskripsi'    => 'Ini penjelasan tentang dinas pertanian di daerah subang',
-            'username'    => 'user',
-            'password'    => 'user'
-        ];
-        $this->db->table('profile')->insert($data);
+        // $data = [
+        //     'logo' => '/img/logo_portal.png',
+        //     'nama_dinas'    => 'Dinas Pertanian Subang',
+        //     'deskripsi'    => 'Ini penjelasan tentang dinas pertanian di daerah subang',
+        //     'username'    => 'user',
+        //     'password'    => 'user'
+        // ];
+
+        $faker = \Faker\Factory::create('id_ID');
+        for ($i = 0; $i < 100; $i++) {
+            $data = [
+                'logo' => $faker->image,
+                'nama_dinas'    => $faker->company,
+                'deskripsi'    => $faker->text,
+                'username'    => $faker->name,
+                'password'    => 'user'
+            ];
+            $this->db->table('profile')->insert($data);
+        }
     }
 }
