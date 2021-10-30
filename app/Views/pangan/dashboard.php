015e8dd7c3bfd5ad86ddaba6d194f9e2db3d3246
ToniSahmir <sahmir.toni66@gmail.com>
2021-10-30 10:58:35 +0700

    mengubah isi conten dashboard

--- app/Views/pangan/dashboard.php
+++ app/Views/pangan/dashboard.php
@@ -27,147 +27,328 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-3">
-          <a href="#">
-            <div class="card bg-info">
+          <div class="card shadow">
+            <a href="">
               <div class="card-body">
                 <div class="text-center">
-                  <img class="img-fluid" src="/img/pngwing.com (13).png">
+                  <img src="/img/ayam.png" class="img-fluid">
                 </div>
-                <h3 class="profile-username text-center">Sayuran</h3>
+                <div class="ribbon-wrapper ribbon-xl">
+                  <div class="ribbon bg-danger text-xl">
+                    Ayam
+                  </div>
+                </div>
               </div>
-            </div>
-          </a>
+            </a>
+          </div>
 
-          <a href="#">
-            <div class="card bg-primary">
-              <div class="card-body">
-                <div class="text-center">
-                  <img class="img-fluid" src="/img/pngwing.com (4).png">
-                </div>
-                <h3 class="profile-username text-center">Sapi</h3>
+          <div class="row">
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </div>
             </div>
-          </a>
-
-          <a href="#">
-            <div class="card bg-secondary">
-              <div class="card-body">
-                <div class="text-center">
-                  <img class="img-fluid" src="/img/kambing.png">
-                </div>
-                <h3 class="profile-username text-center">Kambing</h3>
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </div>
             </div>
-          </a>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div class="col-lg-3">
-          <a href="#">
-            <div class="card bg-warning">
+          <div class="card shadow">
+            <a href="">
               <div class="card-body">
                 <div class="text-center">
-                  <img class="img-fluid" src="/img/domba.png">
+                  <img src="/img/ayam.png" class="img-fluid">
                 </div>
-                <h3 class="profile-username text-center">Domba</h3>
+                <div class="ribbon-wrapper ribbon-xl">
+                  <div class="ribbon bg-danger text-xl">
+                    Ayam
+                  </div>
+                </div>
               </div>
-            </div>
-          </a>
-
-          <a href="#">
-            <div class="card bg-danger">
+            </a>
+          </div>
+          <div class="card shadow">
+            <a href="">
               <div class="card-body">
                 <div class="text-center">
-                  <img class="img-fluid" src="/img/pngwing.com (11).png">
+                  <img src="/img/ayam.png" class="img-fluid">
                 </div>
-                <h3 class="profile-username text-center">Jagung</h3>
+                <div class="ribbon-wrapper ribbon-xl">
+                  <div class="ribbon bg-danger text-xl">
+                    Ayam
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </div>
             </div>
-          </a>
-
-          <a href="#">
-            <div class="card bg-success">
-              <div class="card-body">
-                <div class="text-center">
-                  <img class="img-fluid" src="/img/buah.png">
-                </div>
-                <h3 class="profile-username text-center">Buah-buahan</h3>
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </div>
             </div>
-          </a>
+          </div>
         </div>
 
         <div class="col-lg-3">
-          <a href="#">
-            <div class="card bg-indigo">
+          <div class="card shadow">
+            <a href="">
               <div class="card-body">
                 <div class="text-center">
-                  <img class="img-fluid" src="/img/ayam.png">
+                  <img src="/img/ayam.png" class="img-fluid">
                 </div>
-                <h3 class="profile-username text-center">Ayam</h3>
+                <div class="ribbon-wrapper ribbon-xl">
+                  <div class="ribbon bg-danger text-xl">
+                    Ayam
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </div>
             </div>
-          </a>
-
-          <a href="#">
-            <div class="card bg-navy">
-              <div class="card-body">
-                <div class="text-center">
-                  <img class="img-fluid" src="/img/pngwing.com (14).png">
-                </div>
-                <h3 class="profile-username text-center">Beras</h3>
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </div>
             </div>
-          </a>
-
-          <a href="#">
-            <div class="card bg-fuchsia">
-              <div class="card-body">
-                <div class="text-center">
-                  <img class="img-fluid" src="/img/pngwing.com (10).png">
-                </div>
-                <h3 class="profile-username text-center">Umbi-umbian</h3>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </div>
             </div>
-          </a>
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div class="col-lg-3">
-          <a href="#">
-            <div class="card bg-teal">
-              <div class="card-body">
-                <div class="text-center">
-                  <img class="img-fluid" src="/img/pngwing.com (9).png">
-                </div>
-                <h3 class="profile-username text-center">Kacang-kacangan</h3>
+          <div class="row">
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </div>
             </div>
-          </a>
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/ayam.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Ayam
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
 
-          <a href="#">
-            <div class="card bg-lime">
+          <div class="card shadow">
+            <a href="">
               <div class="card-body">
                 <div class="text-center">
-                  <img class="img-fluid" src="/img/pngwing.com (7).png">
+                  <img src="/img/ayam.png" class="img-fluid">
                 </div>
-                <h3 class="profile-username text-center">Ikan</h3>
+                <div class="ribbon-wrapper ribbon-xl">
+                  <div class="ribbon bg-danger text-xl">
+                    Ayam
+                  </div>
+                </div>
               </div>
-            </div>
-          </a>
+            </a>
+          </div>
+          <div class="card shadow">
+            <a href="">
+              <div class="card-body">
+                <div class="text-center">
+                  <img src="/img/ayam.png" class="img-fluid">
+                </div>
+                <div class="ribbon-wrapper ribbon-xl">
+                  <div class="ribbon bg-danger text-xl">
+                    Ayam
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
 
-          <a href="#">
-            <div class="card bg-orange">
-              <div class="card-body">
-                <div class="text-center">
-                  <img class="img-fluid" src="/img/pngwing.com (8).png">
-                </div>
-                <h3 class="profile-username text-center">Telur</h3>
-              </div>
-            </div>
-          </a>
         </div>
+
       </div>
-      <!-- /.row -->
-    </div><!-- /.container-fluid -->
+    </div>
   </div>
   <!-- /.content -->
 </div>
