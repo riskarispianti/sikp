944fc1c14c9b106c9d12bee0152b481e52c7b3ee
ToniSahmir <sahmir.toni66@gmail.com>
2021-10-30 19:41:25 +0700

    merename file gambar

--- app/Views/pangan/dashboard.php
+++ app/Views/pangan/dashboard.php
@@ -27,15 +27,16 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-3">
-          <div class="card shadow">
-            <a href="">
+          <div class="card rounded-6">
+            <a href="" class="btn btn-outline-info">
               <div class="card-body">
                 <div class="text-center">
-                  <img src="/img/ayam.png" class="img-fluid">
+                  <img src="/img/buah.png" class="img-fluid">
+                  <h4>Buah-buahan</h4>
                 </div>
-                <div class="ribbon-wrapper ribbon-xl">
-                  <div class="ribbon bg-danger text-xl">
-                    Ayam
+                <div class="ribbon-wrapper ribbon-lg">
+                  <div class="ribbon bg-danger text-lg">
+                    Buah-buahan
                   </div>
                 </div>
               </div>
@@ -48,11 +49,11 @@
                 <a href="">
                   <div class="card-body">
                     <div class="text-center">
-                      <img src="/img/ayam.png" class="img-fluid">
+                      <img src="/img/kacang.png" class="img-fluid">
                     </div>
                     <div class="ribbon-wrapper ribbon-lg">
-                      <div class="ribbon bg-danger text-lg">
-                        Ayam
+                      <div class="ribbon bg-danger text-sm">
+                        Kacang-kacangan
                       </div>
                     </div>
                   </div>
@@ -64,11 +65,11 @@
                 <a href="">
                   <div class="card-body">
                     <div class="text-center">
-                      <img src="/img/ayam.png" class="img-fluid">
+                      <img src="/img/ikan.png" class="img-fluid">
                     </div>
                     <div class="ribbon-wrapper ribbon-lg">
                       <div class="ribbon bg-danger text-lg">
-                        Ayam
+                        Ikan
                       </div>
                     </div>
                   </div>
@@ -80,11 +81,11 @@
             <a href="">
               <div class="card-body">
                 <div class="text-center">
-                  <img src="/img/ayam.png" class="img-fluid">
+                  <img src="/img/beras.png" class="img-fluid">
                 </div>
                 <div class="ribbon-wrapper ribbon-xl">
                   <div class="ribbon bg-danger text-xl">
-                    Ayam
+                    Beras
                   </div>
                 </div>
               </div>
@@ -97,11 +98,11 @@
             <a href="">
               <div class="card-body">
                 <div class="text-center">
-                  <img src="/img/ayam.png" class="img-fluid">
+                  <img src="/img/ikan.png" class="img-fluid">
                 </div>
                 <div class="ribbon-wrapper ribbon-xl">
                   <div class="ribbon bg-danger text-xl">
-                    Ayam
+                    Ikan
                   </div>
                 </div>
               </div>
@@ -111,15 +112,134 @@
             <a href="">
               <div class="card-body">
                 <div class="text-center">
-                  <img src="/img/ayam.png" class="img-fluid">
+                  <img src="/img/sayur2.png" class="img-fluid">
                 </div>
                 <div class="ribbon-wrapper ribbon-xl">
-                  <div class="ribbon bg-danger text-xl">
-                    Ayam
+                  <div class="ribbon bg-danger text-lg">
+                    Sayur-sayuran
                   </div>
                 </div>
               </div>
             </a>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/kambing.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Kambing
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/jagung.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Jagung
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3">
+          <div class="card shadow">
+            <a href="">
+              <div class="card-body">
+                <div class="text-center">
+                  <img src="/img/sapi.png" class="img-fluid">
+                </div>
+                <div class="ribbon-wrapper ribbon-xl">
+                  <div class="ribbon bg-danger text-xl">
+                    Sapi
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/sayur.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Sayur
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/kambing.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Kambing
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/telur.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        Telur
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="card shadow">
+                <a href="">
+                  <div class="card-body">
+                    <div class="text-center">
+                      <img src="/img/umbi.png" class="img-fluid">
+                    </div>
+                    <div class="ribbon-wrapper ribbon-lg">
+                      <div class="ribbon bg-danger text-lg">
+                        umbi-umbian
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
           </div>
           <div class="row">
             <div class="col-6">
@@ -158,136 +278,17 @@
         </div>
 
         <div class="col-lg-3">
-          <div class="card shadow">
-            <a href="">
-              <div class="card-body">
-                <div class="text-center">
-                  <img src="/img/ayam.png" class="img-fluid">
-                </div>
-                <div class="ribbon-wrapper ribbon-xl">
-                  <div class="ribbon bg-danger text-xl">
-                    Ayam
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
           <div class="row">
             <div class="col-6">
               <div class="card shadow">
                 <a href="">
                   <div class="card-body">
                     <div class="text-center">
-                      <img src="/img/ayam.png" class="img-fluid">
+                      <img src="/img/domba.png" class="img-fluid">
                     </div>
                     <div class="ribbon-wrapper ribbon-lg">
                       <div class="ribbon bg-danger text-lg">
-                        Ayam
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div class="col-6">
-              <div class="card shadow">
-                <a href="">
-                  <div class="card-body">
-                    <div class="text-center">
-                      <img src="/img/ayam.png" class="img-fluid">
-                    </div>
-                    <div class="ribbon-wrapper ribbon-lg">
-                      <div class="ribbon bg-danger text-lg">
-                        Ayam
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-6">
-              <div class="card shadow">
-                <a href="">
-                  <div class="card-body">
-                    <div class="text-center">
-                      <img src="/img/ayam.png" class="img-fluid">
-                    </div>
-                    <div class="ribbon-wrapper ribbon-lg">
-                      <div class="ribbon bg-danger text-lg">
-                        Ayam
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div class="col-6">
-              <div class="card shadow">
-                <a href="">
-                  <div class="card-body">
-                    <div class="text-center">
-                      <img src="/img/ayam.png" class="img-fluid">
-                    </div>
-                    <div class="ribbon-wrapper ribbon-lg">
-                      <div class="ribbon bg-danger text-lg">
-                        Ayam
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-6">
-              <div class="card shadow">
-                <a href="">
-                  <div class="card-body">
-                    <div class="text-center">
-                      <img src="/img/ayam.png" class="img-fluid">
-                    </div>
-                    <div class="ribbon-wrapper ribbon-lg">
-                      <div class="ribbon bg-danger text-lg">
-                        Ayam
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div class="col-6">
-              <div class="card shadow">
-                <a href="">
-                  <div class="card-body">
-                    <div class="text-center">
-                      <img src="/img/ayam.png" class="img-fluid">
-                    </div>
-                    <div class="ribbon-wrapper ribbon-lg">
-                      <div class="ribbon bg-danger text-lg">
-                        Ayam
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3">
-          <div class="row">
-            <div class="col-6">
-              <div class="card shadow">
-                <a href="">
-                  <div class="card-body">
-                    <div class="text-center">
-                      <img src="/img/ayam.png" class="img-fluid">
-                    </div>
-                    <div class="ribbon-wrapper ribbon-lg">
-                      <div class="ribbon bg-danger text-lg">
-                        Ayam
+                        Domba
                       </div>
                     </div>
                   </div>
@@ -316,11 +317,11 @@
             <a href="">
               <div class="card-body">
                 <div class="text-center">
-                  <img src="/img/ayam.png" class="img-fluid">
+                  <img src="/img/sayur.png" class="img-fluid">
                 </div>
                 <div class="ribbon-wrapper ribbon-xl">
                   <div class="ribbon bg-danger text-xl">
-                    Ayam
+                    Sayur
                   </div>
                 </div>
               </div>
@@ -330,11 +331,11 @@
             <a href="">
               <div class="card-body">
                 <div class="text-center">
-                  <img src="/img/ayam.png" class="img-fluid">
+                  <img src="/img/umbi.png" class="img-fluid">
                 </div>
                 <div class="ribbon-wrapper ribbon-xl">
                   <div class="ribbon bg-danger text-xl">
-                    Ayam
+                    Umbi-umbian
                   </div>
                 </div>
               </div>
