4c0af3f44274fb175ebe2206c6b9df0a6e3265a2
ToniSahmir <sahmir.toni66@gmail.com>
2021-12-19 19:41:33 +0700

    perbaiki error tabel

--- app/Models/PerikananModel.php
+++ app/Models/PerikananModel.php
@@ -9,7 +9,7 @@ class PerikananModel extends Model
     protected $table                = 'prod_ikan';
     protected $primaryKey           = 'id_ikan';
     protected $useAutoIncrement     = true;
-    protected $allowedFields        = ['jenis_ikan', 'jml_prod', 'lama_prod', 'waktu_prod', 'biaya_prod', 'harga_hsl_prod', 'wadah_budidaya', 'sumber_pengairan', 'benih', 'jns_asal_pakan', 'alat_teknologi', 'peman_hsl_prod', 'limbah_hsl_prod', 'nama_anc', 'penanggulangan', 'id_kom', 'id_tp', 'id_sp', 'id_ip', 'id_produsen'];
+    protected $allowedFields        = ['jenis_ikan', 'jml_prod', 'lama_prod', 'waktu_prod', 'biaya_prod', 'harga_hsl_prod', 'wadah_budidaya', 'sumber_pengairan', 'benih', 'jns_asal_pakan', 'alat_teknologi', 'peman_hsl_prod', 'limbah_hsl_prod', 'nama_anc', 'penanggulangan', 'id_kom', 'id_tp', 'id_ip', 'id_produsen'];
 
     // Dates
     protected $useTimestamps        = true;
@@ -20,7 +20,7 @@ class PerikananModel extends Model
             return $this->db->table('prod_ikan')
                 ->join('komoditas', 'komoditas.id_kom=prod_ikan.id_kom')
                 ->join('tempat_prod', 'tempat_prod.id_tp=prod_ikan.id_tp')
-                ->join('sentra_prod', 'sentra_prod.id_sp=prod_ikan.id_sp')
+                // ->join('sentra_prod', 'sentra_prod.id_sp=prod_ikan.id_sp')
                 ->join('infras_prod', 'infras_prod.id_ip=prod_ikan.id_ip')
                 ->join('produsen', 'produsen.id_produsen=prod_ikan.id_produsen')
                 ->get()->getResultArray();
@@ -29,7 +29,7 @@ class PerikananModel extends Model
         return $this->where(['id_ikan' => $id_ikan])
             ->join('komoditas', 'komoditas.id_kom=prod_ikan.id_kom')
             ->join('tempat_prod', 'tempat_prod.id_tp=prod_ikan.id_tp')
-            ->join('sentra_prod', 'sentra_prod.id_sp=prod_ikan.id_sp')
+            // ->join('sentra_prod', 'sentra_prod.id_sp=prod_ikan.id_sp')
             ->join('infras_prod', 'infras_prod.id_ip=prod_ikan.id_ip')
             ->join('produsen', 'produsen.id_produsen=prod_ikan.id_produsen')
             ->first();
--- app/Models/TanamanModel.php
+++ app/Models/TanamanModel.php
@@ -9,7 +9,7 @@ class TanamanModel extends Model
     protected $table = 'prod_tanam';
     protected $primaryKey = 'id_tanam';
     protected $useAutoIncrement = true;
-    protected $allowedFields  = ['jenis_tanam', 'ls_tanam', 'ls_panen', 'produktivitas', 'jml_prod', 'lama_proses_prod', 'perk_iklim', 'perk_tanam_panen', 'biaya_prod', 'harga_jual_prod', 'lahan_prod', 'sumber_pengairan', 'bibit', 'pupuk', 'alat_teknologi', 'peman_hsl_prod', 'limbah_hsl_prod', 'nama_anc', 'penanggulangan', 'id_kom', 'id_tp', 'id_sp', 'id_ip', 'id_produsen'];
+    protected $allowedFields  = ['jenis_tanam', 'ls_tanam', 'ls_panen', 'produktivitas', 'jml_prod', 'lama_proses_prod', 'perk_iklim', 'perk_tanam_panen', 'biaya_prod', 'harga_jual_prod', 'lahan_prod', 'sumber_pengairan', 'bibit', 'pupuk', 'alat_teknologi', 'peman_hsl_prod', 'limbah_hsl_prod', 'nama_anc', 'penanggulangan', 'id_kom', 'id_tp', 'id_ip', 'id_produsen'];
 
     protected $useTimestamps = true;
 
@@ -19,7 +19,7 @@ class TanamanModel extends Model
             return $this->db->table('prod_tanam')
                 ->join('komoditas', 'komoditas.id_kom=prod_tanam.id_kom')
                 ->join('tempat_prod', 'tempat_prod.id_tp=prod_tanam.id_tp')
-                ->join('sentra_prod', 'sentra_prod.id_sp=prod_tanam.id_sp')
+                // ->join('sentra_prod', 'sentra_prod.id_sp=prod_tanam.id_sp')
                 ->join('infras_prod', 'infras_prod.id_ip=prod_tanam.id_ip')
                 ->join('produsen', 'produsen.id_produsen=prod_tanam.id_produsen')
                 ->get()->getResultArray();
@@ -28,7 +28,7 @@ class TanamanModel extends Model
         return $this->where(['id_tanam' => $id_tanam])
             ->join('komoditas', 'komoditas.id_kom=prod_tanam.id_kom')
             ->join('tempat_prod', 'tempat_prod.id_tp=prod_tanam.id_tp')
-            ->join('sentra_prod', 'sentra_prod.id_sp=prod_tanam.id_sp')
+            // ->join('sentra_prod', 'sentra_prod.id_sp=prod_tanam.id_sp')
             ->join('infras_prod', 'infras_prod.id_ip=prod_tanam.id_ip')
             ->join('produsen', 'produsen.id_produsen=prod_tanam.id_produsen')
             ->first();
