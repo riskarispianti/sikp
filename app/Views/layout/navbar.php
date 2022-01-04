9d47451c82790407225340cd0af7d6fca1dc850d
ToniSahmir <sahmir.toni66@gmail.com>
2022-01-04 22:29:11 +0700

    tambah produksi pada menu

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -31,9 +31,9 @@
       <li class="nav-item dropdown active">
         <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Produksi</a>
         <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow" style="left: 0px; right: inherit;">
-          <li><a href="/tanaman" class="dropdown-item">Tanaman Pangan </a></li>
-          <li><a href="/peternakan" class="dropdown-item">Peternakan</a></li>
-          <li><a href="/perikanan" class="dropdown-item">Perikanan</a></li>
+          <li><a href="/tanaman" class="dropdown-item">Produksi Tanaman</a></li>
+          <li><a href="/peternakan" class="dropdown-item">Produksi Peternakan</a></li>
+          <li><a href="/perikanan" class="dropdown-item">Produksi Perikanan</a></li>
         </ul>
       </li>
       <!-- profile Dropdown Menu -->
